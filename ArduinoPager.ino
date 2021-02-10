7e31c3521342eb9bf03dd274c67fe59df0dca332
Simon <16670268+dl1ne@users.noreply.github.com>
2021-02-10 12:05:25 +0100

    Update ArduinoPager.ino
    Added remote setup settings

--- ArduinoPager.ino
+++ ArduinoPager.ino
@@ -8,6 +8,7 @@
 
 #define FREQUENCY     439987500 // 439.9875 MHz
 #define IS_RFM69HCW   true // set to 'true' if you are using an RFM69HCW module
+#define POWER_OUT     15 // power output ranges from 0 (5dBm) to 31 (20dBm)
 
 #define SERIAL_BAUD      38400
 
@@ -19,13 +20,22 @@
 #define PTT_LED       12
 #define STATUS_LED    13
 
-#define MAX_LEN                512
+#define MAX_LEN       512
+
+#define WORD_EOT      0x17
+#define WORD_CFG      0x18
 
 #define FLASH_DELAY   32000U
 RFM69 radio = RFM69(RFM69_CS, RFM69_IRQ, IS_RFM69HCW, RFM69_IRQN);
 
 String message = "";
 boolean tx_mode = false;
+boolean setup_mode = false;
+int setup_step = 0;
+
+uint32_t freq = FREQUENCY;
+int power = POWER_OUT;
+
 char cmessage[MAX_LEN];
 
 bool led = false;
@@ -34,9 +44,14 @@ uint32_t count = 0U;
 void setup() {
   while (!Serial); // wait until serial console is open, remove if not tethered to computer
   Serial.begin(SERIAL_BAUD);
-
-  // Hard Reset the RFM module
   pinMode(RFM69_RST, OUTPUT);
+  pinMode(PTT_LED, OUTPUT);
+  pinMode(STATUS_LED, OUTPUT);
+  setup_radio();
+}
+
+void setup_radio() {
+  // Hard Reset the RFM module
   digitalWrite(RFM69_RST, HIGH);
   delay(100);
   digitalWrite(RFM69_RST, LOW);
@@ -47,18 +62,19 @@ void setup() {
   if (IS_RFM69HCW) {
     radio.setHighPower();    // Only for RFM69HCW & HW!
   }
-  radio.setPowerLevel(31); // power output ranges from 0 (5dBm) to 31 (20dBm)
+  radio.setPowerLevel(power); // power output ranges from 0 (5dBm) to 31 (20dBm)
 
   // Set to ham POCSAQ frequency
-  radio.setFrequency(FREQUENCY);
+  radio.setFrequency(freq);
 
   // Disable encryption
   radio.encrypt(0);
-
-  // Initialize LED pins
-  pinMode(PTT_LED, OUTPUT);
-  pinMode(STATUS_LED, OUTPUT);
+  setup_mode = false;
+  tx_mode = false;
+  setup_step = 0;
+  message = "";
 }
+
 
 void loop() {
   count++;
@@ -69,28 +85,65 @@ void loop() {
   }
 }
 
+
 void serialEventRun(void) {
   if (Serial.available()) serialEvent();
 }
 
+
 void serialEvent() {
   while (Serial.available()) {
-    if (tx_mode == false) {
-      tx_mode = true;
-      radio.set_tx_mode(true);
-      digitalWrite(STATUS_LED, HIGH);
-    }
     char inChar = (char)Serial.read();
-    if (inChar != 0x17) { // end of transmission
+
+    // normal packets
+    if (inChar != WORD_EOT && inChar != WORD_CFG && setup_mode == false) {
+      if (tx_mode == false) {
+        tx_mode = true;
+        radio.set_tx_mode(tx_mode);
+        digitalWrite(STATUS_LED, HIGH);
+      }
       message = (inChar ^= 0xFF);
       message.toCharArray(cmessage,message.length() + 1);
       radio.send_pocsag(cmessage, strlen(cmessage));
     }
-    else {
+
+    // end of packet 
+    if (inChar == WORD_EOT && tx_mode == true) {
       tx_mode = false;
-      radio.set_tx_mode(false);
+      radio.set_tx_mode(tx_mode);
       digitalWrite(STATUS_LED, LOW);
-      Serial.write(0x17);
+      Serial.write(WORD_EOT);
     }
+
+    if (inChar != WORD_CFG && inChar != WORD_EOT && setup_mode == true) {
+      message = message + inChar;
+    }
+
+    // setup step 1, end, set freq
+    if (inChar == WORD_CFG && setup_mode == true && setup_step == 1) {
+      freq = (uint32_t)message[0] << 24 |
+             (uint32_t)message[1] << 16 |
+             (uint32_t)message[2] << 8  |
+             (uint32_t)message[3];
+      message = "";
+      setup_step++;
+    }
+
+    // setup step 2, end, set power
+    if (inChar == WORD_EOT && setup_mode == true && setup_step == 2) {
+      power = message.toInt();
+      message = "";
+      digitalWrite(STATUS_LED, LOW);
+      setup_radio();
+    }
+
+    // enable setup mode
+    if (inChar == WORD_CFG && setup_mode == false && tx_mode == false) {
+      setup_mode = true;
+      setup_step++;
+      message = "";
+      digitalWrite(STATUS_LED, HIGH);
+    }
+    
   }
 }
